cc67e9d051c984b8f96b2c4bef17a664e35354d2
sumanthgdsumu27-collab <sumanthgdsumu27@gmail.com>
2026-02-26 22:47:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -10,40 +10,7 @@ document.addEventListener("DOMContentLoaded", function () {
       top: window.innerHeight,
       behavior: "smooth"
     });
-  }, 2000); // 2 seconds delay
+  }, 4000); // 2 seconds delay
 
 
-  /* ===========================
-     2. SCROLL ARROW FUNCTION
-     =========================== */
-
-  const arrow = document.getElementById("autoScrollArrow");
-
-  if (arrow) {
-
-    // Scroll when arrow is clicked
-    arrow.addEventListener("click", function () {
-      window.scrollBy({
-        top: window.innerHeight,
-        behavior: "smooth"
-      });
-    });
-
-    // Hide arrow after scrolling
-    window.addEventListener("scroll", function () {
-      if (window.scrollY > window.innerHeight / 2) {
-        arrow.style.display = "none";
-      } else {
-        arrow.style.display = "block";
-      }
-    });
-
-  }
-
-});
-
-window.scrollBy({
-  top: window.innerHeight,
-  behavior: "smooth"
-});
 
